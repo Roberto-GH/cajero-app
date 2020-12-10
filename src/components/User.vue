55fba3e0cb7defb09beac9a15d4c0ca0cc175aec
Roberto Londoño <roberthdrums@gmail.com>
2020-12-10 16:38:52 -0500

    Agregando archivos cajero-app vue

---
+++ src/components/User.vue
@@ -0,0 +1,40 @@
+<!-- TEMPLATE representa el html (estrucuctura)-->
+<template>
+  <div id="User">
+    <h2>Hola <span> {{ username }}, </span> ¡Bienvenido!</h2>
+  </div>
+</template>
+
+<!-- SCRIPT representa el js (comportamiento)-->
+<script>
+export default {
+  name: "User",
+  data: function () {
+    return {
+      username: "none",
+    };
+  },
+  created: function () {
+    this.username = this.$route.params.username;
+  },
+};
+</script>
+
+<!-- STYLE representa el css (estilo)-->
+<style>
+#User {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+#User h2 {
+  font-size: 50px;
+  color: #283747;
+}
+#User span {
+  color: crimson;
+  font-weight: bold;
+}
+</style>
